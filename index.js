7371414fd5ad35f5a18c100a63a657b74c8faa4f
Ljubow Neumann <citymnk@gmail.com>
2023-10-09 23:37:53 +0200

    Add task 3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/* 1 */
-console.log('#1: https://w/ww.codewars.com/kata/54a2e93b22d236498400134b /kata/54a2e93b22d236498400134b');
+/* #1 https://www.codewars.com/kata/54a2e93b22d236498400134b */
+
 function presses(phrase) {
   const alphabet = [
     ['1'],
@@ -20,7 +20,45 @@ function presses(phrase) {
 
   for (const symbol of phrase) sum += alphabet[alphabet.findIndex(arr => arr.includes(symbol))].indexOf(symbol) + 1;
 
-  console.log({inpit: phrase, output: sum});
+  return {inpit: phrase, output: sum};
 }
 
-presses('LOL');
+const task1 = presses('LOL');
+
+
+// ==========================
+/* #2 */
+
+function loneliest(str) {
+  str = str.trim();
+  let chars = str.split(/\s*/);
+  let spaces = str.split(/\S/).map(x => x.length);
+  let spacesPerChar = chars.map((c, i) => spaces[i] + spaces[i + 1]);
+  let spaceSize = spacesPerChar.reduce((t, a) => t > a ? t : a);
+  return chars.filter((c, i) => spacesPerChar[i] === spaceSize);
+}
+
+const task2 = loneliest('abc');
+
+
+// ==========================
+/* #3 https://www.codewars.com/kata/588468f3b3d02cf67b0005cd */
+
+function additionWithoutCarrying(a,b) {
+  let max = Math.max(a,b);
+  let min = Math.min(a,b);
+  let output = [];
+
+  if (max === 0) return 0;
+
+  while (max > 0) {
+    let sum = max % 10 + min % 10;
+    output.unshift(sum > 9 ? sum % 10 : sum);
+    max = Math.floor(max / 10);
+    min = Math.floor(min / 10);
+  }
+
+  return +output.join('');
+}
+
+additionWithoutCarrying(456, 1734);
